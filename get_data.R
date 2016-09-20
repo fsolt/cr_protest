b4c06594c6cede5d655a754b0f7ce827ee426996
Frederick Solt <fsolt@mac.com>
2016-09-20 11:15:32 -0500

    add comments; download files only if needed; download old files from archive.org; extract texts

--- get_data.R
+++ get_data.R
@@ -2,8 +2,10 @@ library(readr)
 library(dplyr)
 library(stringr)
 library(rvest)
+library(tidyr)
 library(purrr)
 
+# get training file
 cr <- read_csv("http://www.protestas.iis.ucr.ac.cr/protestas.csv")
 write_csv(cr, "data-raw/protestas.csv")
 
@@ -13,17 +15,19 @@ names(cr) <- names(cr) %>%
     make.names() %>% 
     str_replace_all("\\.", "_")
 
-cron <- read_html("http://www.protestas.iis.ucr.ac.cr/publicaciones/cronologias")
-
-last_page <- cron %>% 
+# get PDF reports -------------------
+# find number of last page of reports
+last_page <- read_html("http://www.protestas.iis.ucr.ac.cr/publicaciones/cronologias") %>% 
     html_nodes(".next a") %>%
     html_attr("href") %>% 
     str_extract("\\d+$") %>% 
     as.numeric()
 
+# build list of all pages of reports
 all_pages <- str_c("http://www.protestas.iis.ucr.ac.cr/publicaciones/cronologias?page=",
                    1:last_page)
 
+# get the (arbitrary) numbers that identify reports from all pages
 report_numbers <- map(all_pages, function(page) {
     read_html(page) %>%
         html_nodes(".caption .btn-primary") %>%
@@ -31,24 +35,84 @@ report_numbers <- map(all_pages, function(page) {
     unlist() %>% 
     str_extract("\\d+")
 
-months <- c("enero", "febrero", "marzo", "abril",
-            "mayo", "junio", "julio", "agosto",
-            "septiembre", "octubre", "noviembre", "diciembre")
+# get report names, formatted to year_month 
+mes <- data_frame(
+    mes = c("Enero", "Febrero", "Marzo", "Abril",
+         "Mayo", "Junio", "Julio", "Agosto",
+         "Septiembre", "Octubre", "Noviembre", "Diciembre"),
+    no = sprintf("%02d", 1:12))
+
+ifelse(is.na(idx <- match(county, pattern)), county, replacement[idx])
 
 report_names <- map(all_pages, function(page) {
     read_html(page) %>%
         html_nodes(".caption h3") %>%
         str_extract("(?<=>)[^<]*")}) %>%
     unlist() %>% 
-    str_replace("(\\w+) (\\d+)", "\\2_\\1") %>% 
-    tolower()
+    as_data_frame() %>% 
+    separate(value, c("mes", "year"), " ") %>% 
+    left_join(mes, by = "mes") %>% 
+    unite(name, year, no) %>% 
+    select(name) %>% 
+    unlist()
 
+# make files directory, if it doesn't already exist
+dir.create("data-raw/files", showWarnings = FALSE, recursive = TRUE) 
+
+# download all reports as PDF files
 walk2(report_numbers, report_names, function(number, name) {
-    doc <- str_c("http://www.protestas.iis.ucr.ac.cr/publicaciones/", number) %>%
-        read_html() %>% 
-        html_node(":nth-child(6) .col-md-12") %>% 
-        str_extract('\\/system[^\\"]+') %>% 
-        str_c("http://www.protestas.iis.ucr.ac.cr", .)
-    download.file(doc, destfile = str_c("data-raw/files/", name, ".pdf"))
+    if (!file.exists(str_c("data-raw/files/", name, ".pdf"))) { # if file doesn't exist
+        doc <- str_c("http://www.protestas.iis.ucr.ac.cr/publicaciones/", number) %>%
+            read_html() %>% 
+            html_node(":nth-child(6) .col-md-12") %>% 
+            str_extract('\\/system[^\\"]+') %>% 
+            str_c("http://www.protestas.iis.ucr.ac.cr", .) # get the link to it
+        download.file(doc, destfile = str_c("data-raw/files/", name, ".pdf")) # and download
+    }
+})
+
+# get earlier reports
+old_links <- read_html("https://web.archive.org/web/20140612020851/http://www.clacso.org.ar/institucional/1h.php?idioma=esp") %>% 
+    html_nodes("#item_11 div .link_osal_inverso") %>%
+    html_attr("href")
+
+old <- data_frame(
+    link = old_links,
+    year = str_extract(link, "\\d{4}$"),
+    mes_todo = str_extract(link, "(?<=%20)[\\w-]+(?=%20\\d{4}$)"),
+    mes = str_extract(link, "(?<=%20)\\w+(?=-?\\w*%20\\d{4}$)")) %>% 
+    left_join(mes, by = "mes") %>% 
+    mutate(name = str_c(year, "_", no))
+
+old_names <- old$name
+
+walk2(old_links, old_names, function(link, name) {
+    if (!file.exists(str_c("data-raw/files/", name, ".pdf"))) { # if file doesn't exist
+        doc <- str_c("http://web.archive.org", link) # create full link
+        while (!file.exists(str_c("data-raw/files/", name, ".pdf"))) { # until file exists,
+            try(download.file(doc, destfile = str_c("data-raw/files/", name, ".pdf"))) # keep trying to download
+            Sys.sleep(4)
+        }
+    }
+})
+
+# convert to text ---------------------------------
+# make texts directory, if it doesn't already exist
+dir.create("data-raw/texts", showWarnings = FALSE) 
+
+# get pdfbox-app if not already present
+if (!file.exists("pdfbox-app-2.0.3.jar")) {
+    download.file("http://www.webhostingjams.com/mirror/apache/pdfbox/2.0.3/pdfbox-app-2.0.3.jar", "pdfbox-app-2.0.3.jar")
+}
+
+all_reports <- c(report_names, old_names)
+
+# extract text from PDFs
+walk(all_reports, function(name) {
+    if (!file.exists(str_c("data-raw/texts/", name, ".txt"))) { # if text doesn't exist
+        str_c("java -jar pdfbox-app-2.0.3.jar ExtractText \"data-raw/files/", 
+                     name, ".pdf\" \"data-raw/texts/", name,".txt\"") %>% 
+            system(ignore.stderr = TRUE)
+    }
 })
 
