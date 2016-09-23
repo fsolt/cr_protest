26d89ef80af514efa68c4c2690f7aaf6200359d2
Frederick Solt <fsolt@mac.com>
2016-09-23 15:01:27 -0500

    recode mass protests missed by tipo_de_protesta

--- R/training_data.R
+++ R/training_data.R
@@ -9,8 +9,8 @@ if (!file.exists("data-raw/protestas.csv")) {
 }
 
 # reformat variable names
-prot <- read_csv("data-raw/protestas.csv")
-names(prot) <- names(prot) %>%
+prot0 <- read_csv("data-raw/protestas.csv")
+names(prot0) <- names(prot0) %>%
     tolower() %>% 
     make.names() %>% 
     str_replace_all("\\.", "_")
@@ -19,17 +19,29 @@ names(prot) <- names(prot) %>%
 mass_protests <- c("Actos sobre la propiedad",
                    "Bloqueo",
                    "Huelga",
+                   "Huelga de hambre",
                    "Marcha",
                    "Mitin o concentración",
                    "Paro",
                    "Toma de propiedad")
 
-prot1 <- prot %>%
+prot <- prot0 %>%
     mutate(mass = if_else(tipo_de_protesta %in% mass_protests, 
                           true = 1,
                           false = 0),
+           sub_tipo_de_protesta = if_else(is.na(sub_tipo_de_protesta), "ninguno", sub_tipo_de_protesta),
            mass = if_else(str_detect(resumen, "bloque\\B") &
-                              !str_detect(resumen, "(Uber|importaciones|FIV)"),
+                              !str_detect(resumen, "(Uber|importaciones|FIV)") &
+                              sub_tipo_de_protesta!="Amenaza",
+                          true = 1,
+                          false = mass),
+           mass = if_else(str_detect(resumen, "se concentra"),
+                          true = 1,
+                          false = mass),
+           mass = if_else(str_detect(resumen, "((realizan?|inician?) una?|(declaran?|mantienen?) en) (paro|huelga)"),
+                          true = 1,
+                          false = mass),
+           mass = if_else(str_detect(resumen, "mantienen? ocupando"),
                           true = 1,
                           false = mass),
            prot_number = min_rank(resumen)) %>% 
