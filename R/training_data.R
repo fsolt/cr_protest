7a8105a83411606ab1fbc583c587f8a2b5aef3ae
Frederick Solt <fsolt@mac.com>
2016-09-23 09:37:43 -0500

    exclude duplicate events (some are coded as more than one type of protest, so have more than one entry)

--- R/training_data.R
+++ R/training_data.R
@@ -24,18 +24,21 @@ mass_protests <- c("Actos sobre la propiedad",
                    "Paro",
                    "Toma de propiedad")
 
-prot <- prot %>%
+prot1 <- prot %>%
     mutate(mass = if_else(tipo_de_protesta %in% mass_protests, 
                           true = 1,
                           false = 0),
            mass = if_else(str_detect(resumen, "bloque\\B") &
                               !str_detect(resumen, "(Uber|importaciones|FIV)"),
                           true = 1,
-                          false = mass))
+                          false = mass),
+           prot_number = min_rank(resumen)) %>% 
+    group_by(resumen) %>% 
+    mutate(mass = max(mass),
+           prot_entry = row_number(resumen)) %>% 
+    ungroup() %>% 
+    filter(prot_entry==1) %>% 
+    select(-prot_entry)
 
-prot <- prot %>% 
-    mutate(bloqueo = (tipo_de_protesta=="Bloqueo") %>% as.numeric(),
-           bloqueo2 = str_detect(resumen, "bloque\\B") &
-               !str_detect(resumen, "(Uber|importaciones|FIV)"))
 
-# 
+
