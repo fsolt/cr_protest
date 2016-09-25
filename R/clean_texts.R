0a8583a3b66ba09a7f28f566f4f8bac9173f3ba0
Frederick Solt <fsolt@mac.com>
2016-09-25 17:37:24 -0500

    correctly recovers month of event (except for 2001)

--- R/clean_texts.R
+++ R/clean_texts.R
@@ -11,7 +11,7 @@ all_text_files <- data_frame(file = list.files("data-raw/texts"),
 text_file_names <- all_text_files %>% 
     select(file) %>% 
     unlist()
-    
+
 texts <- text_file_names %>% 
     str_c("data-raw/texts/", .) %>%
     map(function(x) {
@@ -26,33 +26,33 @@ meses <- c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agos
 
 clean_text <- function(t) {
     t %>% 
-    # two-year-old pre-stringr code; sorry it's (extra-)hard to read 
-    # Add extra line breaks around days
-    gsub(paste0("\\n((?:", paste(días, collapse="|"), ")[[:blank:]]*[0-9]{1,2})\\n"),
-         "\n\n\\1\n\n",  ., ignore.case=T) %>%
-    # Add extra line breaks around months
-    gsub(paste0("\\n((?:", paste(meses, collapse="|"), ")[[:blank:]]*(de [0-9]{4})?)\\n"),
-         "\n\n\n\\1\n\n\n",  ., ignore.case=T) %>% 
-    # Add extra line break after "Glosario de Siglas"
-    gsub("Glosario de Siglas", "Glosario de Siglas\n", ., ignore.case=T) %>% 
-    # Omit line breaks between sentences within paragraphs
-    gsub("([^\n]{70,}\\.)\\n([[:upper:]]|“)", "\\1 \\2", .) %>% 
-    # Omit lines with just page numbers
-    gsub("\\n+\\s*\\d+\\s*\\n+", "\\\n", .) %>% 
-    # Omit lines with headers or cite-to footnote
-    gsub("\\n[^\n]*(Cronolog|OSAL|Osal|IIS)[^\n]*\\n", "\\\n", .) %>% 
-    str_replace_all("\\n(\\s*Protesta\\s*)?Social[^\n]*\\n\\s*http://iis.ucr.ac.cr/[^\n]*\\n", "") %>% 
-    # Omit line breaks within sentences
-    gsub("([][:alpha:]),;:”%&\"])[[:blank:]]*\\n+\\s*([[:alnum:](“«\"$])", "\\1 \\2", .) %>% 
-    # Omit line breaks at numbers within sentences
-    gsub("([[:digit:]])[[:blank:]]*\\n+\\s*([[:lower:]])", "\\1 \\2", .) %>% 
-    # Omit line breaks within words
-    gsub("([[:alpha:][:digit:]])\\s*\\-\\s*\\n\\s*([[:alpha:][:digit:]])", "\\1\\2", .) %>% 
-    # Re-split month-day-date lines     
-    str_replace_all(str_c("\\n\\s*(", str_c(meses, collapse="|"),
-                ")\\s*((", str_c(días, collapse="|"),
-                ")\\s*\\d{1,2})\\s*\\n"),
-                "\n\n\\1\n\n\n\\2\n\n")
+        # two-year-old pre-stringr code; sorry it's (extra-)hard to read 
+        # Add extra line breaks around days
+        gsub(paste0("\\n((?:", paste(días, collapse="|"), ")[[:blank:]]*[0-9]{1,2})\\n"),
+             "\n\n\\1\n\n",  ., ignore.case=T) %>%
+        # Add extra line breaks around months
+        gsub(paste0("\\n((?:", paste(meses, collapse="|"), ")[[:blank:]]*(de [0-9]{4})?)\\n"),
+             "\n\n\n\\1\n\n\n",  ., ignore.case=T) %>% 
+        # Add extra line break after "Glosario de Siglas"
+        gsub("Glosario de Siglas", "Glosario de Siglas\n", ., ignore.case=T) %>% 
+        # Omit line breaks between sentences within paragraphs
+        gsub("([^\n]{70,}\\.)\\n([[:upper:]]|“)", "\\1 \\2", .) %>% 
+        # Omit lines with just page numbers
+        gsub("\\n+\\s*\\d+\\s*\\n+", "\\\n", .) %>% 
+        # Omit lines with headers or cite-to footnote
+        gsub("\\n[^\n]*(Cronolog|OSAL|Osal|IIS)[^\n]*\\n", "\\\n", .) %>% 
+        str_replace_all("\\n(\\s*Protesta\\s*)?Social[^\n]*\\n\\s*http://iis.ucr.ac.cr/[^\n]*\\n", "") %>% 
+        # Omit line breaks within sentences
+        gsub("([][:alpha:]),;:”%&\"])[[:blank:]]*\\n+\\s*([[:alnum:](“«\"$])", "\\1 \\2", .) %>% 
+        # Omit line breaks at numbers within sentences
+        gsub("([[:digit:]])[[:blank:]]*\\n+\\s*([[:lower:]])", "\\1 \\2", .) %>% 
+        # Omit line breaks within words
+        gsub("([[:alpha:][:digit:]])\\s*\\-\\s*\\n\\s*([[:alpha:][:digit:]])", "\\1\\2", .) %>% 
+        # Re-split month-day-date lines     
+        str_replace_all(str_c("\\n\\s*(", str_c(meses, collapse="|"),
+                              ")\\s*((", str_c(días, collapse="|"),
+                              ")\\s*\\d{1,2})\\s*\\n"),
+                        "\n\n\\1\n\n\n\\2\n\n")
 }
 
 día_pattern <- días %>% 
@@ -81,7 +81,8 @@ cleaned_texts <- map2_df(texts, text_file_names, function(ts, t_f) {
         left_join(data_frame(mes = meses,
                              mes_mm = sprintf("%02d", 1:12)), by = "mes") %>% 
         zoo::na.locf() %>% 
-        mutate(día_fecha = str_replace(día_fecha, "^\\s*", "")) %>% 
+        mutate(mm = ifelse(is.na(mes_mm), mm, mes_mm), 
+               día_fecha = str_replace(día_fecha, "^\\s*", "")) %>%
         filter(!(is.na(día_fecha) |
                      día_fecha=="Glosario de Siglas" |
                      día_fecha==resumen |
@@ -90,27 +91,24 @@ cleaned_texts <- map2_df(texts, text_file_names, function(ts, t_f) {
                                   "Jueves 13",
                                   día_fecha)) %>% 
         separate(día_fecha, c("día", "dd")) %>%
-        mutate(dd = sprintf("%02d", as.numeric(dd)))  
+        mutate(dd = sprintf("%02d", as.numeric(dd))) %>% 
+        select(file, yyyy, mm, dd, día, resumen)
 })
-
- 
-
-    
-    
 
 
 # geography
-# cantons <- "https://en.wikipedia.org/wiki/Cantons_of_Costa_Rica" %>% 
-#     read_html() %>% 
-#     html_nodes(xpath = '//*[@id="mw-content-text"]/table[2]') %>% 
-#     html_table() %>% 
-#     first()
-# 
-# names(cantons) <- c("cantón", "provincia", 
-#                     "pop11", "pop00", "pop_change",
-#                     "area", "pop_density", "incorporation")
-# matches <- rep("\\1", length(cantons$cantón))
-# names(matches) <- str_c(".*(", cantons$cantón, ").*")
+cantons <- "https://en.wikipedia.org/wiki/Cantons_of_Costa_Rica" %>%
+    read_html() %>%
+    html_nodes(xpath = '//*[@id="mw-content-text"]/table[2]') %>%
+    html_table() %>%
+    first()
+
+names(cantons) <- c("cantón", "provincia",
+                    "pop11", "pop00", "pop_change",
+                    "area", "pop_density", "incorporation")
+matches <- rep("\\1", length(cantons$cantón))
+names(matches) <- str_c(".*(", cantons$cantón, ").*")
+
 # 
 # no_geo <- no_geo %>% mutate(cant = str_replace_all(resumen, matches),
 #                             cant = ifelse(cant!=resumen, cant, NA_character_))
