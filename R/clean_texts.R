19769382fffe9a461929fbb0e5b66fd860e99a26
Frederick Solt <fsolt@mac.com>
2016-09-25 07:44:26 -0500

    work towards cleaning all texts using map2_df

--- R/clean_texts.R
+++ R/clean_texts.R
@@ -5,14 +5,14 @@ library(stringr)
 dir.create("data-raw/texts_clean", showWarnings = FALSE) # Make texts_clean directory if it doesn't already exist
 
 all_text_files <- data_frame(file = list.files("data-raw/texts"),
-                             year = str_extract(file, "\\d{4}"))
+                             year = str_extract(file, "\\d{4}"),
+                             month = str_extract(file, "(?<=_)\\d{2}"))
 
-text_files <- all_text_files %>% 
-    filter(year>=2008) %>% 
+text_file_names <- all_text_files %>% 
     select(file) %>% 
-    unlist
+    unlist()
     
-texts <- text_files %>% 
+texts <- text_file_names %>% 
     str_c("data-raw/texts/", .) %>%
     map(function(x) {
         t0 <- readBin(x, file.info(x)$size, what="raw") # Work around to account for embedded nuls in text files
@@ -21,19 +21,18 @@ texts <- text_files %>%
     })
 
 # Clean
-días <- c("Domingo", "Lunes", "Martes", "Miércoles", "Miercoles", "Jueves", "Viernes", "Sábado", "Sabado") 
-    # str_c(collapse="|") %>% 
-    # str_c("^(",.,")\\s*\\d{1,2}")
+días <- c("Domingo", "Lunes", "Martes", "Mi[ée]rcoles", "Jueves", "Viernes", "S[áa]bado") 
 meses <- c("Enero", "Febrero", "Marzo", "Mayo", "Abril", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
-#gsub(paste0("\\n((?:", paste(days, collapse="|"), ")[[:blank:]]*[0-9]{1,2})\\n"), "\n\n\\1\n\n",  t, ignore.case=T)
 
-t <- texts[[86]] %>% 
+clean_text <- function(t) {
+    t %>% 
     # two-year-old pre-stringr code; sorry it's (extra-)hard to read 
     # Add extra line breaks around days
     gsub(paste0("\\n((?:", paste(días, collapse="|"), ")[[:blank:]]*[0-9]{1,2})\\n"),
          "\n\n\\1\n\n",  ., ignore.case=T) %>%
     # Add extra line breaks around months
-    gsub(paste0("\\n((?:", paste(meses, collapse="|"), ")[[:blank:]]*(de [0-9]{4})?)\\n"), "\n\n\\1\n\n",  ., ignore.case=T) %>% 
+    gsub(paste0("\\n((?:", paste(meses, collapse="|"), ")[[:blank:]]*(de [0-9]{4})?)\\n"),
+         "\n\n\n\\1\n\n\n",  ., ignore.case=T) %>% 
     # Add extra line break after "Glosario de Siglas"
     gsub("Glosario de Siglas", "Glosario de Siglas\n", ., ignore.case=T) %>% 
     # Omit line breaks between sentences within paragraphs
@@ -42,55 +41,78 @@ t <- texts[[86]] %>%
     gsub("\\n+\\s*\\d+\\s*\\n+", "\\\n", .) %>% 
     # Omit lines with headers or cite-to footnote
     gsub("\\n[^\n]*(Cronolog|OSAL|Osal|IIS)[^\n]*\\n", "\\\n", .) %>% 
-    str_replace("\\n(\\s*Protesta\\s*)?Social[^\n]*\\n\\s*http://iis.ucr.ac.cr/[^\n]*\\n", "") %>% 
+    str_replace_all("\\n(\\s*Protesta\\s*)?Social[^\n]*\\n\\s*http://iis.ucr.ac.cr/[^\n]*\\n", "") %>% 
     # Omit line breaks within sentences
     gsub("([][:alpha:]),;:”%&\"])[[:blank:]]*\\n+\\s*([[:alnum:](“«\"$])", "\\1 \\2", .) %>% 
     # Omit line breaks at numbers within sentences
     gsub("([[:digit:]])[[:blank:]]*\\n+\\s*([[:lower:]])", "\\1 \\2", .) %>% 
     # Omit line breaks within words
-    gsub("([[:alpha:][:digit:]])\\s*\\-\\s*\\n\\s*([[:alpha:][:digit:]])", "\\1\\2", .)  
-writeLines(t, "t.txt")
-
-
-
-    
-    
-
-
-# for (i in seq(length(all.texts))) {
-#     text.file <- paste0("../Texts/", all.texts[i])
-#     #  t <- readChar(text.file, file.info(text.file)$size) # Doesn't work because of embedded nuls in some text files (e.g., 100.txt)
-#     t0 <- readBin(text.file, file.info(text.file)$size, what="raw") # Work around to account for embedded nuls in text files
-#     t <- rawToChar(t0[t0!="00"])
-    # 
-    # days <- c("Domingo", "Lunes", "Martes", "Miércoles", "Miercoles", "Jueves", "Viernes", "Sábado", "Sabado")
-    # days2 <- unlist(lapply(days, function(i) paste(unlist(strsplit(i, split="")), collapse=" ")))
-    t2 <- gsub(paste0("\\n((?:", paste(days, collapse="|"), ")[[:blank:]]*[0-9]{1,2})\\n"), "\n\n\\1\n\n",  t, ignore.case=T) # Add extra line breaks around days
-    # t2 <- gsub(paste0("((?:", paste(days2, collapse="|"), ")[[:blank:]]*[0-9])([[:blank:]]*[0-9])?"), "\n\n\\1\\2\n\n",  t2, ignore.case=T) # Add extra line breaks around days
-    months <- c("Enero", "Febrero", "Marzo", "Mayo", "Abril", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
-    # months2 <- unlist(lapply(months, function(i) paste(unlist(strsplit(i, split="")), collapse=" ")))
-    t2 <- gsub(paste0("\\n((?:", paste(months, collapse="|"), ")[[:blank:]]*(de [0-9]{4})?)\\n"), "\n\n\\1\n\n",  t2, ignore.case=T) # Add extra line breaks around months
-    # t2 <- gsub(paste0("((?:", paste(months2, collapse="|"), ")[[:blank:]]*(de [0-9]{4})?)"), "\n\\1.\n",  t2, ignore.case=T) # Add extra line breaks around months
-    t2 <- gsub("Glosario de Siglas", "Glosario de Siglas\n", t2, ignore.case=T)
-    
-    t2 <- gsub("([^\n]{75}\\.)\\n([[:upper:]])", "\\1 \\2", t2)   # Omit line breaks between sentences within paragraphs
-    t2 <- gsub("\\n+\\s*\\d+\\s*\\n+", "\\\n", t2)   # Omit lines with just page numbers
-    t2 <- gsub("\\n[^\n]*(Cronolog|OSAL|Osal)[^\n]*\\n", "\\\n", t2)  # Omit lines with headers
-    t2 <- gsub("([[:alpha:]),:])[[:blank:]]*\\n+\\s*([[:alnum:](“])", "\\1 \\2", t2) # Omit line breaks within sentences
-    t2 <- gsub("([[:digit:]])[[:blank:]]*\\n+\\s*([[:lower:]])", "\\1 \\2", t2) # Omit line breaks at numbers within sentences
-    t2 <- gsub("([[:alpha:]])\\s*\\-\\s*\\n\\s*([[:alpha:]])", "\\1\\2", t2)  # Omit line breaks within words
-    
-    t2 <- gsub("Glosario de Siglas", "Glosario de Siglas\n", t2, ignore.case=T)
-    writeLines(t2, paste0("../Clean_Texts/", all.texts[i]))
+    gsub("([[:alpha:][:digit:]])\\s*\\-\\s*\\n\\s*([[:alpha:][:digit:]])", "\\1\\2", .) %>% 
+    # Re-split month-day-date lines     
+    str_replace_all(str_c("\\n\\s*(", str_c(meses, collapse="|"),
+                ")\\s*((", str_c(días, collapse="|"),
+                ")\\s*\\d{1,2})\\s*\\n"),
+                "\n\n\\1\n\n\n\\2\n\n")
 }
 
-# geography
-cantons <- "https://en.wikipedia.org/wiki/Cantons_of_Costa_Rica" %>% 
-    read_html() %>% 
-    html_nodes(xpath = '//*[@id="mw-content-text"]/table[2]') %>% 
-    html_table() %>% 
-    first()
+día_pattern <- días %>% 
+    str_c(collapse="|") %>% 
+    str_c("^\\s*((",.,")\\s*\\d{1,2})|(Glosario de Siglas)")
 
-names(cantons) <- c("cantón", "provincia", 
-                    "pop11", "pop00", "pop_change",
-                    "area", "pop_density", "incorporation")
+mes_pattern <- meses %>% 
+    str_c(collapse="|") %>% 
+    str_c("^\\s*((",.,")\\s*)")
+
+cleaned_texts <- map2_df(texts, text_file_names, function(ts, t_f) {
+    clean_text(ts) %>%
+        str_split("\\n") %>%
+        first() %>% 
+        data_frame(file = t_f,
+                   yyyy = str_extract(file, "\\d{4}"),
+                   mm = str_extract(file, "(?<=_)\\d{2}"),
+                   resumen = .,
+                   día_fecha = ifelse(str_detect(resumen, día_pattern),
+                                      str_extract(resumen, día_pattern),
+                                      NA_character_),
+                   mes = ifelse(str_detect(resumen, mes_pattern),
+                                str_extract(resumen, mes_pattern),
+                                NA_character_)) %>% 
+        filter(!str_detect(resumen, "^\\s*$")) %>% 
+        zoo::na.locf() %>% 
+        mutate(día_fecha = str_replace(día_fecha, "^\\s*", "")) %>% 
+        filter(!(is.na(día_fecha) |
+                     día_fecha=="Glosario de Siglas" |
+                     día_fecha==resumen |
+                     mes==resumen)) %>%
+        mutate(día_fecha = ifelse(día_fecha=="Jueves13", 
+                                  "Jueves 13",
+                                  día_fecha) #,
+               # mm = ifelse(mm!=which(meses %in% mes) & !is.na(mes),
+               #             which(meses %in% mes),
+               #             mm)
+               ) %>% 
+        separate(día_fecha, c("día", "dd")) %>%
+        mutate(dd = sprintf("%02d", as.numeric(dd)))  
+})
+
+ 
+
+    
+    
+
+
+# geography
+# cantons <- "https://en.wikipedia.org/wiki/Cantons_of_Costa_Rica" %>% 
+#     read_html() %>% 
+#     html_nodes(xpath = '//*[@id="mw-content-text"]/table[2]') %>% 
+#     html_table() %>% 
+#     first()
+# 
+# names(cantons) <- c("cantón", "provincia", 
+#                     "pop11", "pop00", "pop_change",
+#                     "area", "pop_density", "incorporation")
+# matches <- rep("\\1", length(cantons$cantón))
+# names(matches) <- str_c(".*(", cantons$cantón, ").*")
+# 
+# no_geo <- no_geo %>% mutate(cant = str_replace_all(resumen, matches),
+#                             cant = ifelse(cant!=resumen, cant, NA_character_))
