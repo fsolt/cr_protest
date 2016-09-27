604ce32766c8d8b5731bc18f320b903ee54290f6
Frederick Solt <fsolt@mac.com>
2016-09-27 13:23:08 -0500

    progress

--- R/training_data.R
+++ R/training_data.R
@@ -9,8 +9,8 @@ if (!file.exists("data-raw/protestas.csv")) {
 }
 
 # reformat variable names
-prot0 <- read_csv("data-raw/protestas.csv")
-names(prot0) <- names(prot0) %>%
+iis0 <- read_csv("data-raw/protestas.csv")
+names(iis0) <- names(iis0) %>%
     tolower() %>% 
     make.names() %>% 
     str_replace_all("\\.", "_")
@@ -25,29 +25,47 @@ mass_protests <- c("Actos sobre la propiedad",
                    "Paro",
                    "Toma de propiedad")
 
-prot <- prot0 %>%
+iis <- iis0 %>%
     mutate(mass = if_else(tipo_de_protesta %in% mass_protests, 
                           true = 1,
                           false = 0),
-           sub_tipo_de_protesta = if_else(is.na(sub_tipo_de_protesta), "ninguno", sub_tipo_de_protesta),
-           mass = if_else(str_detect(resumen, "(?<!no )bloque\\B") &
-                              !str_detect(resumen, "(Uber|importaciones|FIV)") &
-                              sub_tipo_de_protesta!="Amenaza",
+           sub_tipo_de_protesta = if_else(is.na(sub_tipo_de_protesta),
+                                          "ninguno", 
+                                          sub_tipo_de_protesta)) %>% 
+    mutate(mass = if_else(str_detect(resumen, "((?<!no )bloque\\B)|(cierre de calles)|tortuguismo") &
+                              !str_detect(resumen, "[Aa]menaz|[Aa]nuncian?\\b"),
+                          true = 1,
+                          false = 0),
+           mass = if_else(str_detect(resumen, "(se concentran?)|([Cc]on una) concentración"),
                           true = 1,
                           false = mass),
-           mass = if_else(str_detect(resumen, "se concentra|[Cc]on una concentración"),
+           mass = if_else(str_detect(resumen, "((realizan?|inician?) una?|(declaran?|encuentran?) en|[Cc]on una?|(La|El)|este día se espera el|mantienen? (el|la|en)|cumple el \\b.*\\b día de) (paro|huelga)"),
                           true = 1,
                           false = mass),
-           mass = if_else(str_detect(resumen, "((realizan?|inician?) una?|(declaran?|mantienen?) en) (paro|huelga)"),
+           mass = if_else(str_detect(resumen, "el paro (de labores )?realizado"),
                           true = 1,
                           false = mass),
-           mass = if_else(str_detect(resumen, "mantienen? ocupando"),
+           mass = if_else(str_detect(resumen, "(mantienen? (ocupando|cerrado))|(se amarran?\\b)|(impiden? el acceso)"),
                           true = 1,
                           false = mass),
-           mass = if_else(str_detect(resumen, "[Ll]uego de la manifestación"),
+           mass = if_else(str_detect(resumen, "([Rr]ealizan?|[Dd]urante) (el|la|una?) (manifestación|marcha|paro|huelga|actividad pública)"),
                           true = 1,
                           false = mass),
-           prot_number = min_rank(resumen)) %>% 
+           mass = if_else(str_detect(resumen, "(([Ll]uego de la)|([Cc]on una)|(realizan?)( una?)?) (manifestación|mitin|caravana)"),
+                          true = 1,
+                          false = mass),
+           mass = if_else(str_detect(resumen, "protestan? (frente|antes|en las afueras|en las calles)"),
+                          true = 1,
+                          false = mass),
+           mass = if_else(str_detect(resumen, "realización de una manifestación|manifestación convocada días atrás|se presentan en las oficinas"),
+                          true = 1,
+                          false = mass),
+           mass = if_else(str_detect(resumen, "marchan?\\b|toman? las calles"),
+                          true = 1,
+                          false = mass),
+           mass = if_else(str_detect(resumen, "cierran? (el portón|los portones)"),
+                          true = 1,
+                          false = mass)) %>% 
     group_by(resumen) %>% 
     mutate(mass = max(mass),
            prot_entry = row_number(resumen)) %>% 
@@ -55,65 +73,54 @@ prot <- prot0 %>%
     filter(prot_entry==1) %>% 
     select(-prot_entry)
 
-prot4 <- cleaned_texts %>% 
-    filter(file=="2011_04.txt") %>% 
-    mutate(mass = if_else(str_detect(resumen, "(?<!no )bloque\\B") &
-                              !str_detect(resumen, "[Aa]menaz"),
-                          true = 1,
-                          false = 0),
-           mass = if_else(str_detect(resumen, "se concentra|[Cc]on una (concentración)"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "((realizan?|inician?) una?|(declaran?|mantienen?) en|[Cc]on una?) (paro|huelga)"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "mantienen? (ocupando|cerrado)"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "realizan? (el|la|una?) (manifestación|marcha|paro|huelga)"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "[Ll]uego de la manifestación"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "protestan? (frente|antes|en las afueras)"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "manifestación convocada días atrás"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "marchan?\\b"),
-                          true = 1,
-                          false = mass))
+hand_checked <- c("2008_01.txt", 
+                  "2009_02.txt", 
+                  "2010_03.txt",
+                  "2011_04.txt",
+                  "2012_05.txt",
+                  "2013_06.txt",
+                  "2014_07.txt") %>% 
+    map_df(function(m) {
+        cleaned_texts %>% 
+            filter(file==m) %>% 
+            mutate(mass = if_else(str_detect(resumen, "((?<!no )bloque\\B)|(cierre de calles)|tortuguismo") &
+                                      !str_detect(resumen, "[Aa]menaz|[Aa]nuncian?\\b"),
+                                  true = 1,
+                                  false = 0),
+                   mass = if_else(str_detect(resumen, "(se concentran?)|([Cc]on una) concentración"),
+                                  true = 1,
+                                  false = mass),
+                   mass = if_else(str_detect(resumen, "((realizan?|inician?) una?|(declaran?|encuentran?) en|[Cc]on una?|(La|El)|este día se espera el|mantienen? (el|la|en)|cumple el \\b.*\\b día de) (paro|huelga)"),
+                                  true = 1,
+                                  false = mass),
+                   mass = if_else(str_detect(resumen, "(el paro (de labores )?realizado)|(paralizan? sus labores)"),
+                                  true = 1,
+                                  false = mass),
+                   mass = if_else(str_detect(resumen, "(mantienen? (ocupando|cerrado))|(se amarran?\\b)|(impiden? el acceso)"),
+                                  true = 1,
+                                  false = mass),
+                   mass = if_else(str_detect(resumen, "([Rr]ealizan?|[Dd]urante) (el|la|una?) (manifestación|marcha|paro|huelga|actividad pública)"),
+                                  true = 1,
+                                  false = mass),
+                   mass = if_else(str_detect(resumen, "(([Ll]uego de la)|([Cc]on una)|(realizan?)( una?)?) (manifestación|mitin|caravana)"),
+                                  true = 1,
+                                  false = mass),
+                   mass = if_else(str_detect(resumen, "(protestan?|manifiestan?) (frente|antes|en las afueras|en las calles)"),
+                                  true = 1,
+                                  false = mass),
+                   mass = if_else(str_detect(resumen, "realización de una manifestación|manifestación convocada días atrás|se presentan? (en las oficinas|al edificio)"),
+                                  true = 1,
+                                  false = mass),
+                   mass = if_else(str_detect(resumen, "marchan?\\b|toman? las calles"),
+                                  true = 1,
+                                  false = mass),
+                   mass = if_else(str_detect(resumen, "cierran? (el portón|los portones)"),
+                                  true = 1,
+                                  false = mass))
+    })
 
-prot5 <- cleaned_texts %>% 
-    filter(file=="2012_05.txt") %>% 
-    mutate(mass = if_else(str_detect(resumen, "(?<!no )bloque\\B") &
-                              !str_detect(resumen, "[Aa]menaz"),
-                          true = 1,
-                          false = 0),
-           mass = if_else(str_detect(resumen, "se concentra|[Cc]on una (concentración)"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "((realizan?|inician?) una?|(declaran?|mantienen?) en|[Cc]on una?) (paro|huelga)"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "mantienen? (ocupando|cerrado)"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "realizan? (el|la|una?) (manifestación|marcha|paro|huelga)"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "[Ll]uego de la manifestación"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "protestan? (frente|antes|en las afueras)"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "manifestación convocada días atrás"),
-                          true = 1,
-                          false = mass),
-           mass = if_else(str_detect(resumen, "marchan?\\b"),
-                          true = 1,
-                          false = mass))
-
+hand_checked2 <- list(iis, hand_checked) %>% 
+    map_df(function(df) {
+        df %>% 
+            select(resumen, mass)
+    })
