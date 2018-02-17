4e9d650f28e5afea8cf54f635c7578edaae2949f
Frederick Solt <fsolt@mac.com>
2018-02-17 10:18:39 -0600

    k-fold cross validation of iis data; train on iis to predict hand_checked

--- R/train_and_classify_quanteda.R
+++ R/train_and_classify_quanteda.R
@@ -1,52 +1,86 @@
 library(tidyverse)
 library(quanteda)
 
-
+# create quanteda corpus of IIS data
 set.seed(324)
-iis_rnd <- hand_checked2 %>% 
+iis_rnd <- iis %>% select(resumen, mass) %>% 
     sample_n(., size=nrow(.), replace=FALSE) # randomize order
-
-iis_train <- iis_rnd %>% 
-    filter(row_number() <= floor(.8*nrow(iis_rnd)))
-iis_test <- iis_rnd %>% 
-    filter(row_number() > floor(.8*nrow(iis_rnd)))
 
 iis_corpus <- corpus(iis_rnd$resumen %>% tolower()) # create quanteda corpus
 docvars(iis_corpus) <- iis_rnd$mass   # attach class labels to the corpus text
 
-# includes stopwords in ngrams . . .
-# iis_dfm <- dfm(iis_corpus, 
-#                tolower = TRUE, 
-#                remove = stopwords("es"),
-#                remove_punct = TRUE,
-#                ngrams = 1:3) %>% 
-#     dfm_trim(min_count = 2, min_docfreq = 2)
-
-# removing stopwords before constructing ngrams
+# remove stopwords, construct ngrams, and build document-feature matrix
 iis_dfm <- tokens(iis_corpus, 
-                     what = "word",
-                     remove_punct = TRUE,
-                     remove_numbers = TRUE) %>% 
+                  what = "word",
+                  remove_punct = TRUE,
+                  remove_numbers = TRUE) %>% 
     tokens_remove(stopwords("es")) %>% 
     tokens_ngrams(n = 1:3) %>% 
     dfm(verbose = FALSE) %>% 
-    dfm_trim(min_count = 2, min_docfreq = 2)
+    dfm_trim(min_docfreq = 10)
 
-iis_dfm_train <- iis_dfm[1:nrow(iis_train), ]
-iis_dfm_test<- iis_dfm[(nrow(iis_train)+1):nrow(iis_rnd), ] 
+# k-fold cross validation
+k <- 10
+folds <- cut(seq(1, nrow(iis_rnd)), breaks = k, labels = FALSE)
 
-nb_classifier <- textmodel_nb(x = iis_dfm_train, 
-                              y = iis_dfm_train@docvars$docvar1,
+correctly_predicted <- vector("double", k)
+for (i in 1:k) {
+    test_rows <- which(folds == i)
+    iis_dfm_test <- iis_dfm[test_rows, ] 
+    iis_dfm_train <- iis_dfm[-test_rows, ]
+    
+    nb_classifier <- textmodel_nb(x = iis_dfm_train, 
+                                  y = iis_dfm_train@docvars$docvar1,
+                                  prior = "docfreq",
+                                  distribution = "Bernoulli")
+    
+    # nb_coefs <- coef(nb_classifier) %>% 
+    #     as_tibble(rownames = "ngram") %>% 
+    #     arrange(-`1`)
+    
+    pred <- predict(nb_classifier, iis_dfm_test)
+    
+    # use pred$nb.predicted to extract the class labels
+    # table(predicted = pred$nb.predicted, actual = iis_test %>% pull("mass")) # confusion matrix
+    correctly_predicted[i] <- mean(pred$nb.predicted == iis_dfm_test@docvars$docvar1)*100
+}
+
+mean(correctly_predicted)
+
+
+# Train on IIS data, test on FS hand-coded sample
+
+all_cr_corpus <- corpus(all_coded_cr$resumen %>% tolower()) # create quanteda corpus
+docvars(all_cr_corpus) <- all_coded_cr$mass   # attach class labels to the corpus text
+
+# remove stopwords, construct ngrams, and build document-feature matrix
+all_cr_dfm <- tokens(all_cr_corpus, 
+                  what = "word",
+                  remove_punct = TRUE,
+                  remove_numbers = TRUE) %>% 
+    tokens_remove(stopwords("es")) %>% 
+    tokens_ngrams(n = 1:3) %>% 
+    dfm(verbose = FALSE) %>% 
+    dfm_trim(min_docfreq = 10)
+
+test_rows <- seq(nrow(iis) + 1, nrow(all_coded_cr))
+all_cr_dfm_test <- all_cr_dfm[test_rows, ] 
+all_cr_dfm_train <- all_cr_dfm[-test_rows, ]
+
+nb_classifier <- textmodel_nb(x = all_cr_dfm_train, 
+                              y = all_cr_dfm_train@docvars$docvar1,
                               prior = "docfreq",
                               distribution = "Bernoulli")
-
-nb_coefs <- coef(nb_classifier) %>% 
-    as_tibble(rownames = "ngram") %>% 
+    
+nb_coefs <- coef(nb_classifier) %>%
+    as_tibble(rownames = "ngram") %>%
     arrange(-`1`)
-
-
-pred <- predict(nb_classifier, iis_dfm_test)
-
+    
+pred <- predict(nb_classifier, all_cr_dfm_test)
+    
 # use pred$nb.predicted to extract the class labels
-table(predicted = pred$nb.predicted, actual = iis_test %>% pull("mass"))
-mean(pred$nb.predicted == iis_test %>% pull("mass"))*100
+table(predicted = pred$nb.predicted, actual = all_cr_dfm_test@docvars$docvar1) # confusion matrix
+mean(pred$nb.predicted == all_cr_dfm_test@docvars$docvar1)*100
+
+hand_checked$pred <- pred$nb.predicted
+
